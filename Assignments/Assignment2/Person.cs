6a3d65c792549c7352bf636daca8ab78d2489fb8
Sarang <sarangmore9860@gmail.com>
2024-07-17 19:06:28 +0530

    Assignment2

---
+++ Assignments/Assignment2/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Person : IComparable<Person>
+    {
+        string name;
+        int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"Name : {this.name}  Age : {this.age}";
+        }
+
+        public int CompareTo(Person other)
+        {
+            if (this.age > other.age) { return 1; }
+            else if (this.age < other.age) { return -1; }
+            else { return 0; }
+        }
+    }
+}
