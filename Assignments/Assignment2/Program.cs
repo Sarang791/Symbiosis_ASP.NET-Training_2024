58470c277fec711a1c079f47c18c25ce1ce1a886
Sarang <sarangmore9860@gmail.com>
2024-07-17 19:08:03 +0530

    assignment2

---
+++ Assignments/Assignment2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>() { new Person("A", 22), new Person("B", 23), new Person("C", 21) };
+
+            Console.WriteLine("Before Sorting : ");
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person);
+            }
+
+            people.Sort();
+
+            Console.WriteLine("\nAfter Sorting on Age : ");
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
